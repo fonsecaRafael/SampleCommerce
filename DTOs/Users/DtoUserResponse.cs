614a42f4de7905249c568633b156aa7cc47a6ca6
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2026-04-30 18:21:43 +0200

    Refat: Changes User Dto to possibility login

--- DTOs/Users/DtoUserResponse.cs
+++ DTOs/Users/DtoUserResponse.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
         public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public bool Seller { get; set; }
         public string? Iva { get; set; }
         public string? TradingName { get; set; }
